0e44360cb838e0c4d1c69223e1ce5d11b40dee3e
Rodel <rodel.crisosto7@gmail.com>
2024-08-17 14:03:00 +0800

    chore: removed feedpost

--- src/features/feed/FeedPosts.tsx
+++ src/features/feed/FeedPosts.tsx
@@ -11,16 +11,6 @@ type FeedPostsProps = {
 const FeedPosts = ({ data }: FeedPostsProps) => {
   return (
     <>
-      <PostCard
-        id={1}
-        user_id={5}
-        audience="public"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci inventore dolor earum autem temporibus cum dignissimos, impedit blanditiis minima delectus aut facere perferendis, voluptate provident?"
-        likes={0}
-        comments={0}
-        shares={0}
-      />
-
       {/* {data?.map(props => {
         return <UserPostCard key={props.id} {...props} />;
       })} */}
