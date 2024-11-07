070805ed838fc6635aadd58e3c6b2005b3c8d530
Rodel <rodel.crisosto7@gmail.com>
2024-11-07 13:54:19 +0800

    chore: add async to params 02

--- src/app/(post)/post/[uuid]/page.tsx
+++ src/app/(post)/post/[uuid]/page.tsx
@@ -1,7 +1,13 @@
 import PostView from '@/features/post/PostView';
 
-const PostPage = ({ params }: { params: { uuid: string } }) => {
-  return <PostView uuid={params.uuid} />;
+interface Params {
+  uuid: string;
+}
+
+const PostPage = async ({ params }: { params: Promise<Params> }) => {
+  const { uuid } = await params;
+
+  return <PostView uuid={uuid} />;
 };
 
 export default PostPage;
