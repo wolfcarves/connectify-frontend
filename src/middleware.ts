acdc6f765556211ad2dc3eb833ffc354a5f7c234
Rodel <rodel.crisosto7@gmail.com>
2024-11-07 15:39:59 +0800

    test: commented some lines in middelware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,32 +2,34 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse;
+  // const response = NextResponse;
 
-  const baseURL = request.url;
-  const pathname = request.nextUrl.pathname;
+  // const baseURL = request.url;
+  // const pathname = request.nextUrl.pathname;
 
-  const sessionCookie = await request.cookies.get('auth_session');
+  // const sessionCookie = await request.cookies.get('auth_session');
 
-  const fetchSession = await fetch(
-    'http://localhost:5000/api/v1/auth/session',
-    {
-      headers: {
-        cookie: `${sessionCookie?.name}=${sessionCookie?.value}`,
-      },
-    },
-  );
+  // const fetchSession = await fetch(
+  //   'http://localhost:5000/api/v1/auth/session',
+  //   {
+  //     headers: {
+  //       cookie: `${sessionCookie?.name}=${sessionCookie?.value}`,
+  //     },
+  //   },
+  // );
 
-  const session = await fetchSession.json();
-  const isSessionValid = session.error?.statusCode !== 403;
+  // const session = await fetchSession.json();
+  // const isSessionValid = session.error?.statusCode !== 403;
 
-  const authRoutes = ['/login', '/signup'];
+  // const authRoutes = ['/login', '/signup'];
 
-  if (isSessionValid && authRoutes.includes(pathname)) {
-    return response.redirect(new URL('/', baseURL));
-  }
+  // if (isSessionValid && authRoutes.includes(pathname)) {
+  //   return response.redirect(new URL('/', baseURL));
+  // }
 
-  return response.next();
+  // return response.next();
+
+  return NextResponse.next();
 }
 
 export const config = {
